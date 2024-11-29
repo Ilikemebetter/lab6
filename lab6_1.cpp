2cc7c5fe85ff2719dce1ed78a5cc2c8a06f200e6
Ilikemebetter <pleum.41733@gmail.com>
2024-11-29 16:34:15 +0700

    lab6_1.cpp completed

--- lab6_1.cpp
+++ lab6_1.cpp
@@ -1,9 +1,27 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 
-int main(){
-    cout << "Enter an integer: ";
-    cout << "#Even numbers = ";
-    cout << "#Odd numbers = ";
-    return 0;
+int main()
+{
+    int N;
+    int isOdd, isEven = 0;
+    while (true)
+    {
+        cout << "Enter an integer: ";
+        cin >> N;
+        if (N == 0)
+        {
+            cout << "#Even numbers = " << isEven << endl;
+            cout << "#Odd numbers = " << isOdd << endl;
+            break;
+        }
+        if (N % 2 == 0)
+        {
+            isEven++;
+        }
+        else
+        {
+            isOdd++;
+        }
+    }
 }
